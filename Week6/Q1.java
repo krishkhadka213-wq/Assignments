2cb9279d1b3b2047f49ca60605db77a45449ab90
Krish Khadka <Krishkhadka213@gmail.com>
2025-12-24 16:03:52 +0545

    Week6 and Scenratio

---
+++ Week6/Q1.java
@@ -0,0 +1,19 @@
+package Week6;
+
+
+/**
+ * Write a description of class Q1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Q1
+{
+  public static void main(String[] args)
+  {
+      int age[]; //declaration
+      age = new int[5]; //construction
+      
+      int [] agr = new int[5]; // declaration and construction
+  }
+}
